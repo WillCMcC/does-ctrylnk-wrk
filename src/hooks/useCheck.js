d418f7a289a875be7392edccd44a87a45724c03a
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2020-11-07 18:04:51 -0800

    invalidate

--- src/hooks/useCheck.js
+++ src/hooks/useCheck.js
@@ -1,10 +1,10 @@
-import { useQuery } from "react-query";
+import { useQuery, queryCache } from "react-query";
 import axios from "axios";
 
 const getOffers = async () => {
 
   const { data: offers } = await axios.get('/offers');
-
+  queryCache.invalidateQueries("best")
   return offers.offersList;
 };
 
