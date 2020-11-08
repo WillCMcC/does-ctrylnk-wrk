107de423ab2ba5034e4e5bb3c3bce94ec3749933
Will McCambley <willmccambley@Wills-MacBook-Pro-2.local>
2020-11-07 17:57:52 -0800

    fixit

--- index.js
+++ index.js
@@ -59,19 +59,13 @@ app.get("/offers", async function (req, res) {
 
   for (let offerIndex in offers.offersList) {
     let offer = offers.offersList[offerIndex];
-    console.log();
+
     let offerRow;
+    let existingOffer;
+
     try {
-      offerRow = await axios.post(
-        "https://apime.dev/api/centurylink_offer",
-        {
-          centurylink_id: offer.catalogId,
-          description: offer.description,
-          price: offer.price,
-          speed: offer.downloadSpeed,
-          mbps: offer.downloadSpeedMbps,
-          fiber: offer.gfastFlag,
-        },
+      existingOffer = await axios.get(
+        `https://apime.dev/api/centurylink_offer?centurylink_id=eq.${offer.catalogId}`,
         {
           headers: {
             Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJQSG1QTVBHU3JwNjdITllXVzVwSDd5emd5QmRqTG1NWHZjckFaR0RiZnRGeTR0Zml6Rm9LY1pqdTdFQmtEbzdLSFpLUEZKaGJaUG1GZiI6InQtYjhmY2EyOGYtYTdjYi00ZWY2LWI4NmEtYzE4YTc5NzMxZjUxIiwiSG12TE1nMnFlUDNuSzVWbTV0TVE3RFQzeVd2NmtIRXB1dkJZMmhnYWVSR0dMandTTjRzTDU4MmZXdU5ZM3p6UlY4aGZGSG96Q1BjbkQiOiI3NmExYzk0OS1hNDljLTQwYjgtYWVjOS0xNmVlMzA4OTMyZmMiLCJQVUhpOXdvcEh3VmZ4OUJQR1c1RGI1SEtob3lHUGJDb2pBaEFOSHlmb2lGNFRnRTJEQlNGbnR6RWh2bTdacGlTcVc2ekRaaUpvaHU0QiI6dHJ1ZSwiV0ZEWWROb3ZZb1F4dnJWeFI0dmFiQXVoc0g5dHRnbUszVVVod0hVOHd0RGR5alJnSlE0NFBCMjVSY0hZQUFERGtvZWdTZnU4ZXlCcVQiOnRydWUsIlk1NThkalNWeHN0ZWczUGtBaDhCUUVaeFBLVGJuc1FKaFcyOXVldER6U0JTM3o4NWtZeG5rRVZHRHBabW9EMlJLdXZ0NHI1ZENhbW93Ijp0cnVlLCJzaWduZWQiOnRydWUsImlhdCI6MTYwMTAwMTQ2NX0.yrD48Ogj-6ydFDu7ML-H7Jo8DNk_Ae3McHrGiBBMttM`,
@@ -81,6 +75,30 @@ app.get("/offers", async function (req, res) {
     } catch (error) {
       console.log(error.message);
     }
+
+    if(!existingOffer.data[0]){
+      try {
+        offerRow = await axios.post(
+          "https://apime.dev/api/centurylink_offer",
+          {
+            centurylink_id: offer.catalogId,
+            description: offer.description,
+            price: offer.price,
+            speed: offer.downloadSpeed,
+            mbps: offer.downloadSpeedMbps,
+            fiber: !!offer.gfastFlag,
+          },
+          {
+            headers: {
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJQSG1QTVBHU3JwNjdITllXVzVwSDd5emd5QmRqTG1NWHZjckFaR0RiZnRGeTR0Zml6Rm9LY1pqdTdFQmtEbzdLSFpLUEZKaGJaUG1GZiI6InQtYjhmY2EyOGYtYTdjYi00ZWY2LWI4NmEtYzE4YTc5NzMxZjUxIiwiSG12TE1nMnFlUDNuSzVWbTV0TVE3RFQzeVd2NmtIRXB1dkJZMmhnYWVSR0dMandTTjRzTDU4MmZXdU5ZM3p6UlY4aGZGSG96Q1BjbkQiOiI3NmExYzk0OS1hNDljLTQwYjgtYWVjOS0xNmVlMzA4OTMyZmMiLCJQVUhpOXdvcEh3VmZ4OUJQR1c1RGI1SEtob3lHUGJDb2pBaEFOSHlmb2lGNFRnRTJEQlNGbnR6RWh2bTdacGlTcVc2ekRaaUpvaHU0QiI6dHJ1ZSwiV0ZEWWROb3ZZb1F4dnJWeFI0dmFiQXVoc0g5dHRnbUszVVVod0hVOHd0RGR5alJnSlE0NFBCMjVSY0hZQUFERGtvZWdTZnU4ZXlCcVQiOnRydWUsIlk1NThkalNWeHN0ZWczUGtBaDhCUUVaeFBLVGJuc1FKaFcyOXVldER6U0JTM3o4NWtZeG5rRVZHRHBabW9EMlJLdXZ0NHI1ZENhbW93Ijp0cnVlLCJzaWduZWQiOnRydWUsImlhdCI6MTYwMTAwMTQ2NX0.yrD48Ogj-6ydFDu7ML-H7Jo8DNk_Ae3McHrGiBBMttM`,
+            },
+          }
+        );
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    
   }
   offers.offersList = offers.offersList.map((offer) => {
     return {
